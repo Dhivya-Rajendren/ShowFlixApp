c788d9e1e72c37696735ffcbf5e78b2b94a6a65d
Dhivya-Rajendren <rajendren.dhivya@gmail.com>
2022-12-31 09:35:51 +0530

    Upload the image in root folder

--- ShowFlixApp/Controllers/ShowsController.cs
+++ ShowFlixApp/Controllers/ShowsController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using ShowFlixApp.Models;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ShowFlixApp.Controllers
 {
     public class ShowsController : Controller
     {
         SQLServerShowRepository repo;
+        private readonly IHostingEnvironment environment;
+
+        public ShowsController(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+        }
         public IActionResult Index()
         {
             repo = new SQLServerShowRepository();
@@ -43,8 +52,18 @@ namespace ShowFlixApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateShow(Show show)
+        public IActionResult CreateShow(Show show,IFormFile file)
         {
+            string wwwPath = this.environment.WebRootPath;// wwwroot/
+
+            string contentPath = Path.Combine(this.environment.WebRootPath, "images");
+            string fileName = Path.GetFileName(file.FileName);
+            using (FileStream stream = new FileStream(Path.Combine(contentPath, fileName), FileMode.Create))
+            {
+                file.CopyTo(stream);
+                show.ShowImg = "images/" + file.FileName;
+            }
+
             repo = new SQLServerShowRepository();
             repo.AddNewShow(show);
             return RedirectToAction("Index");
